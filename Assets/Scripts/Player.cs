cb72caf3857583ac9dd48b298ae890ce08b5748d
UOC\2404270 <averychristian604@gmail.com>
2025-01-14 10:45:38 +0000

    Commit 7: Player script started without movement functional.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float moveX = Input.GetAxis("Horizontal");
-    float moveY = Input.GetAxis("Vertical");
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+
+    public void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        rb.velocity = new Vector2(moveX,moveY)*Time.deltaTime;
+    }
 }
