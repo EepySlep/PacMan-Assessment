74e8563ddeada4f659893ba2396391d346ce94b8
UOC\2404270 <averychristian604@gmail.com>
2025-01-14 16:03:35 +0000

    Commit 16: Added additional comments to the pellets collection for a detailed explanation into functionality.

--- Assets/Scripts/GhostMove.cs
+++ Assets/Scripts/GhostMove.cs
@@ -4,15 +4,5 @@ using UnityEngine;
 
 public class GhostMove : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
--- Assets/Scripts/ItemCollect.cs
+++ Assets/Scripts/ItemCollect.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class ItemCollect : MonoBehaviour
 {
 
-    //This checks if the player is over a pellet and then deletes it if this happens. The print function displays a debug message to the console, which was a functionality test.
+    //This checks if the player is over a pellet and then deletes it if this happens.
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.tag == "Collectible") ;
+        if (other.gameObject.tag == "Collectible") ; //All objects stored under this tag are susceptible to being interacted with by the player.
         {
-            print("Collected");
-            gameObject.active = false;
+            print("Collected"); //The print function displays a debug message to the console, which was a functionality test.
+            gameObject.active = false; //Delete the object interacted with.
         }
     }
 }
