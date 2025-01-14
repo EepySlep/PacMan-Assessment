b9c3c2316e498f11dcd59c1ed875258c4bbd17d3
UOC\2404270 <averychristian604@gmail.com>
2025-01-14 15:44:42 +0000

    Commit 15: Added comments to the pellet collection.

--- Assets/Scripts/ItemCollect.cs
+++ Assets/Scripts/ItemCollect.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ItemCollect : MonoBehaviour
 {
+
+    //This checks if the player is over a pellet and then deletes it if this happens. The print function displays a debug message to the console, which was a functionality test.
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Collectible") ;
