853406db3aa5cd968bb3eaae3fdf1012ad828a43
UOC\2404270 <averychristian604@gmail.com>
2025-01-14 15:03:29 +0000

    Commit 14: Added pellet consumption with debug.

--- Assets/Scripts/ItemCollect.cs
+++ Assets/Scripts/ItemCollect.cs
@@ -4,15 +4,12 @@ using UnityEngine;
 
 public class ItemCollect : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter2D(Collider2D other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        if (other.gameObject.tag == "Collectible") ;
+        {
+            print("Collected");
+            gameObject.active = false;
+        }
     }
 }
