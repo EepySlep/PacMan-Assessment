fde61b343b140a1130d1b72299fbac8872c39e75
UOC\2404270 <averychristian604@gmail.com>
2025-01-14 10:10:05 +0000

    Commit 6: Starting Player Input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    float moveX = Input.GetAxis("Horizontal");
+    float moveY = Input.GetAxis("Vertical");
+}
