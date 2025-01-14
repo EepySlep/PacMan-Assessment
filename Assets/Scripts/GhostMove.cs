9af6da85ad97aa2306567cb93de95cd003469f89
UOC\2404270 <averychristian604@gmail.com>
2025-01-14 17:02:08 +0000

    Commit 18: Started enemy programming. Moves to the left.

--- Assets/Scripts/GhostMove.cs
+++ Assets/Scripts/GhostMove.cs
@@ -4,8 +4,20 @@ using UnityEngine;
 
 public class GhostMove : MonoBehaviour
 {
-    public Transform Waypoint1;
-    public Transform Waypoint2;
-
+    private Vector3 moveDirection; //Stores movement direction.
     public float Speed = 2f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Way2") ; //Checks if the object has met an object and turns the other way once collided.
+        {
+            moveDirection = Vector3.left;
+        }
+
+    }
+
+    private void Update()
+    {
+        transform.Translate(moveDirection * Speed * Time.deltaTime);
+    }
 }
