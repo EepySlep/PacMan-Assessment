17a4a20f06bd28d6801d4556bf36ae09018d9e61
UOC\2404270 <averychristian604@gmail.com>
2025-01-14 16:36:49 +0000

    Commit 17: Added a player death system when interacting with the ghost object.

---
+++ Assets/PlayerDeath.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDeath : MonoBehaviour
+{
+    //This will be called when the player collides with an enemy, ensuring an instant kill.
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy")) //Collision checks for a specific tag, since it is looking for the enemy object to collide with.
+        {
+            // Destroy the player object instantly
+            gameObject.active = false; //See ItemCollect for the same function.
+        }
+    }
+}
--- Assets/Scripts/GhostMove.cs
+++ Assets/Scripts/GhostMove.cs
@@ -4,5 +4,8 @@ using UnityEngine;
 
 public class GhostMove : MonoBehaviour
 {
+    public Transform Waypoint1;
+    public Transform Waypoint2;
 
+    public float Speed = 2f;
 }
